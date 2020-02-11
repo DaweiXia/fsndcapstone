a8035ce521558b33cc13c8ba41e160ce65577639
xiadw <xiadw_public@163.com>
2020-02-11 20:41:25 +0800

    first migration

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_migrate import Migrate
+from models import db, setup_db
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
-
+    setup_db(app)
+    migrate = Migrate(app, db)
     return app
