41fab8d115e75e006979b9a81d49da5ac2375570
xiadw <xiadw_public@163.com>
2020-02-11 21:31:16 +0800

    add delete movie

--- models.py
+++ models.py
@@ -32,6 +32,10 @@ class Movie(db.Model):
         db.session.add(self)
         db.session.commit()
 
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
 
 class Actor:
     pass
