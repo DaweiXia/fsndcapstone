0958e23f901cffae2e348d8d6dcbf5595784d9f0
xiadw <xiadw_public@163.com>
2020-02-11 21:19:46 +0800

    finish post movie endpoint

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, request, abort, jsonify
 from flask_migrate import Migrate
-from models import db, setup_db
+from models import db, setup_db, Movie
+
+from datetime import datetime
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
     setup_db(app)
     migrate = Migrate(app, db)
+
+    @app.route('/movies', methods=['POST'])
+    def post_movies():
+        data = request.json
+        release_date = datetime.now()
+        try:
+            movie = Movie(title=data['title'], release_date=release_date)
+            movie.insert()
+            return jsonify({'success': True})
+        except Exception:
+            db.session.rollback()
+            abort(422)
+
     return app
