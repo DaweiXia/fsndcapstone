d3e03ae59d5592447ad04ba35190cf418acc588d
xiadw <xiadw_public@163.com>
2020-02-16 12:15:05 +0800

    delete database_path variable

--- models.py
+++ models.py
@@ -3,12 +3,11 @@ import os
 from sqlalchemy import Column, String, Integer, DateTime
 from flask_sqlalchemy import SQLAlchemy
 
-database_path = os.environ['DATABASE_URL']
 db = SQLAlchemy()
 
 
 def setup_db(app, database_path=database_path):
-    app.config['SQLALCHEMY_DATABASE_URI'] = database_path
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
