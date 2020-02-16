c1e146aedc110fb93b09048b3a716eb1000ed83a
xiadw <xiadw_public@163.com>
2020-02-16 20:25:48 +0800

    set other envrioment variables at setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
 export FLASK_APP=app.py
 export DATABASE_URL=postgresql://postgres:123456@localhost:5432/casting_agency_test
+export AUTH0_DOMAIN=xiadw.auth0.com
+export ALGORITHMS=['RS256']
+export API_AUDIENCE=casting
